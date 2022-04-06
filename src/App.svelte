085752debfa9a3897a683c7650b92ab149880490
Shawn Lee <changoloco.evg@gmail.com>
2022-04-06 15:46:28 -0500

    :tada: [feat]: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<!-- Title -->
+Get insights that help your business grow.
+<!-- Paragraph -->
+Discover the benefits of data analytics and make better decisions regarding revenue, customer experience,
+and overall efficiency.
+<!-- Stats -->
+10k+ companies 314 templates 12m+ queries
